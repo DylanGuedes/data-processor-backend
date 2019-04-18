5a3158143cc2c8484a2911fb9a355b2879fd2933
DylanGuedes <djmgguedes@gmail.com>
2019-04-18 11:07:38 -0300

    adds seed reset
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,26 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+
+alias DataProcessorBackend.Repo
+alias DataProcessorBackend.InterSCity.JobScript
+alias DataProcessorBackend.InterSCity.JobTemplate
+alias DataProcessorBackend.InterSCity.ProcessingJob
+alias DataProcessorBackend.InterSCity.ScriptSamples.Kmeans
+
+Repo.delete_all ProcessingJob
+Repo.delete_all JobTemplate
+Repo.delete_all JobScript
+
+# KMeans
+attrs = %{
+  title: "KMeans",
+  language: Kmeans.language,
+  code: Kmeans.code,
+  path: "kmeans.py"}
+{:ok, script} = attrs |> JobScript.create()
+
+{:ok, template} =
+  %{title: Kmeans.example_title, user_params: Kmeans.example_user_params}
+  |> JobTemplate.create(script)
